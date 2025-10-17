5eadddd8fd25e66fbed4dd85034175dc8118923f
Dipak Pulami Magar <dpm.it24800@gmail.com>
2025-10-17 20:41:18 +0545

    Add example of list and string enumeration
    Prints elements of a list and enumerates characters in strings.

---
+++ python-codes/python-lists/list-example.py
@@ -0,0 +1,10 @@
+list = [1, 20, 30.56, 434.45, "Ramesh", "Basnet"]
+
+print(f"list[4]: {list[4]}")
+for x, y in enumerate(list[4]):
+    print(f"list[4][{x}]: {y}")
+
+print()
+print(f"list[5]: {list[5]}")
+for m, n in enumerate(list[5]):
+    print(f"list[5][{m}]: {n}")
