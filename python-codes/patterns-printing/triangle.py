1422fc95be0ca4d3f135256c41932a62c94434f2
dpm24800 <dpm.it24800@gmail.com>
2025-10-17 20:50:03 +0545

    triagle pattern with output alignment

--- python-codes/patterns-printing/triangle.py
+++ python-codes/patterns-printing/triangle.py
@@ -1,3 +1,18 @@
 x = int(input("Number: "))
 for i in range (1, x + 1):
-    print("x"*i)
+    print("x "*i)
+
+# Input: 8
+# Output:
+"""
+
+x 
+x x 
+x x x 
+x x x x 
+x x x x x 
+x x x x x x 
+x x x x x x x 
+x x x x x x x x
+
+"""
